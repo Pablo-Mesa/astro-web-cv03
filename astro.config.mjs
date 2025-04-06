887f89174e25de4f653e4c3cb9601cf0f57dc01c
pablomesa <pablomesa1985@gamil.com>
2025-04-06 10:15:07 -0300

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:"https://pablomesa.netlify.app/",
+    base: "astro-web-cv03"
+});
